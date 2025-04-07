1117bdf190dcf02c1cc6d7bf62a3d0de52f7f4f8
raxracks <multisniperism@gmail.com>
2025-04-07 20:08:18 +1200

    fix xmake project targets

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,17 @@ add_requires("raygui 4.0");
 
 target("main")
     set_kind("binary")
-    add_files("src/**.cpp")
+    add_files("src/main.cpp")
+    add_packages("raylib")
+    add_packages("raygui")
+    set_languages("c11", "c++23")
+    set_symbols("debug")
+    
+    set_optimize("fastest")
+    
+target("main2")
+    set_kind("binary")
+    add_files("src/main2.cpp")
     add_packages("raylib")
     add_packages("raygui")
     set_languages("c11", "c++23")
